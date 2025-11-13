d9250b3f0b78df07f738d462f48cb7cc0b4d8774
Hitesh Navare <navarehitesh65@gmail.com>
2025-11-13 23:41:21 +0530

    added the routes for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import { Header } from "./Utils/Header";
+import { LoginCard } from "./Utils/LoginCard";
+import { Routes, Route } from "react-router-dom";
+import Start from "./Utils/Start";
+import { SignupCard } from "./Utils/SignupCard";
 
 function App() {
 
   return (
     <div className="bg-[#121212] text-white h-screen w-screen">
-      <Header />
+   
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<LoginCard />} />
+        <Route path="/signup" element={<SignupCard />} />
+
+      </Routes>
     </div>
   )
 }
